81d15b853d5d4689ac91f5674b7531c1aedf5e04
abhi3899 <abhism3899@gmail.com>
2020-08-29 10:02:12 +0530

    initial commit

---
+++ src/Compnents/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import './Header.css'
+
+const Header =() =>{
+    return(
+        <div>
+            <header>
+            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+             alt="Problem?" />
+             <p className="header-title">Meme Generator</p>
+            </header>
+        </div>
+    )
+
+}
+export default Header
